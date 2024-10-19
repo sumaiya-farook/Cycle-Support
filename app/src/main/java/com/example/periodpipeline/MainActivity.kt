68a16a544304663a745470e457cbcf5323db0549
sumaiyafarook <sfaro5@illinois.edu>
2024-10-19 18:07:10 -0500

    Basic Map feature added

--- app/src/main/java/com/example/periodpipeline/MainActivity.kt
+++ app/src/main/java/com/example/periodpipeline/MainActivity.kt
@@ -1,33 +1,32 @@
 package com.example.periodpipeline
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(),  OnMapReadyCallback {
-//    private lateinit var map: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { R.layout.activity_main }
+        setContentView( R.layout.activity_main )
         val mapFragment = supportFragmentManager
-            .findFragementById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("Marker")
+        )
     }
 }
 
-
-
-override fun onMapReady(googleMap: GoogleMap) {
-    googleMap.addMarker(
-        MarkerOptions()
-            .position(LatLng(0.0, 0.0))
-            .title("Marker")
-    )
-}
 //@Composable
 //fun Greeting(name: String, modifier: Modifier = Modifier) {
 //    Text(
